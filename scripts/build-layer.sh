8bcbd7eda2c0f0b41a182132a6508ee0d445bf76
feliperamis <36742027+feliperamis@users.noreply.github.com>
2023-05-09 16:22:02 +0200

    Update build-layer.sh

--- scripts/build-layer.sh
+++ scripts/build-layer.sh
@@ -6,21 +6,9 @@ set -e
 [ -z "$PACKAGES_LOCATION" ] && PACKAGES_LOCATION=dist
 [ -z "$ARTIFACT_NAME" ]     && ARTIFACT_NAME=sharedModules
 
-echo "--- Layers Packaging [2/4] Installing production packages..."
-npm install --frozen-lockfile --prod --ignore-scripts
-
-echo "--- Layers Packaging [3/4] Running 'node-prune'..."
-mkdir -p nodejs $PACKAGES_LOCATION
+rm -rf node_modules
+npm install
 cp -r node_modules nodejs
-./node-prune nodejs/node_modules
-rm -rf $ARTIFACT_NAME.zip
-
-echo "--- Layers Packaging [4/4] Packaging '$ARTIFACT_NAME'..."
-# Delete aws-sdk because AWS already has it in the runtime
-rm -rf nodejs/node_modules/aws-sdk || :
-rm -rf nodejs/node_modules/typescript || :
-zip -rq $ARTIFACT_NAME.zip nodejs
-
-echo "--- Layers Packaging [4/4] Done!"
-cp $ARTIFACT_NAME.zip $PACKAGES_LOCATION/
-rm -rf nodejs && rm node-prune
+rm -rf nodejs/node_modules/@aws-sdk
+rm -rf nodejs/node_modules/typescript
+zip -rq layer.zip nodejs
